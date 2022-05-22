af2a8e934663b24503535ff9104df4e7b7d90361
Serm135 <pinilloss@uninorte.edu.co>
2022-05-22 17:20:27 -0500

    Login w Hash

--- models/user.model.js
+++ models/user.model.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose'
 const userSchema = new mongoose.Schema({
     username: {type: String, required:true},
     password: {type: String, required:true},
+    salt:{type:String, required:true},
     email: {type: String, required:true},
     birthdate: {type: String, required:true},
     bio: {type: String, required:true}
