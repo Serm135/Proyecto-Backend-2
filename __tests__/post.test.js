edc49dd5f7cb77b7979d93abbda6a8e2ea3d0e27
Serm135 <pinilloss@uninorte.edu.co>
2022-05-30 15:53:50 -0500

    Jest Post 2

--- __tests__/post.test.js
+++ __tests__/post.test.js
@@ -1,10 +1,13 @@
 import app from '../app.js';
 import request from 'supertest';
 import mongoose from "mongoose";
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 describe("post route tests", () => {
     beforeAll(() => {
-        const url = "mongodb+srv://pinilloss:gokuque@cluster0.49scg.mongodb.net/picshar-db?retryWrites=true&w=majority"
+        const url = process.env.DATABASE_URL
         mongoose.connect(url);
     })
 
@@ -46,6 +49,46 @@ describe("post route tests", () => {
             post_id: '62929215f15dd388a1f896a6'
         }
         const {status,_body:body} = await request(app).post('/posts/save').send(info)
+        expect(status).toBe(202)   
+        expect(body.message).toBe('Ok') 
+    })
+
+    test('/saved-by', async () => {
+        const user = {
+            username: 'pinilloss',
+            password: "1234"
+        }
+        await request(app).post('/users/login').send(user)
+        const {status,_body:body} = await request(app).get('/posts/saved-by')
+        expect(status).toBe(202)   
+        expect(body.message).toBe('Ok') 
+    })
+
+    test('/comment', async () => {
+        const user = {
+            username: 'pinilloss',
+            password: "1234"
+        }
+        await request(app).post('/users/login').send(user)
+        const info = {
+            post_id: '62929215f15dd388a1f896a6',
+            comment: 'Confirmo, petrosky moment'
+        }
+        const {status,_body:body} = await request(app).post('/posts/').send(info)
+        expect(status).toBe(202)   
+        expect(body.message).toBe('Ok') 
+    })
+
+    test('/info', async () => {
+        const user = {
+            username: 'pinilloss',
+            password: "1234"
+        }
+        await request(app).post('/users/login').send(user)
+        const info = {
+            post_id: '62929215f15dd388a1f896a6'
+        }
+        const {status,_body:body} = await request(app).get('/posts/').send(info)
         expect(status).toBe(202)   
         expect(body.message).toBe('Ok') 
     })
--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -31,7 +31,7 @@ export const create_comment = async (req,res) => {
                 let comments = dataDB.comments
                 comments.push(data.comment)
                 Post.updateOne({_id:data.post_id},{$set:{comments:comments}}).then(r =>{
-                    res.status(202).json({})
+                    res.status(202).json({message:'Ok'})
                 }).catch(e =>{
                     console.log(e)
                     res.status(500)
@@ -48,7 +48,7 @@ export const create_comment = async (req,res) => {
     
 }
 export const information = async (req,res) => {
-    if (!req.query) {
+    if (!req.query || req.query!='') {
         const data = req.body
         if (data.post_id.length==24) {
             await Post.findOne({_id:data.post_id}).then(dataDB => {
@@ -60,9 +60,9 @@ export const information = async (req,res) => {
                         likes: dataDB.likes.length,
                         comments: dataDB.comments
                     }
-                    res.json(post_info)
+                    res.status(202).json({message:'Ok',post_info})
                 } else {
-                    res.status(404).json('No se encontró la publicación')
+                    res.status(404).json({message:'No se encontró la publicación'})
                 }
             }).catch(e=>{
                     console.log(e)
@@ -71,7 +71,7 @@ export const information = async (req,res) => {
                     })
                 })
         }else{
-            res.status(404).json('No se encontró la publicación')
+            res.status(404).json({message:'No se encontró la publicación'})
         }
     } else {
         const data = req.query
@@ -83,7 +83,7 @@ export const information = async (req,res) => {
                 res.status(404).json(e)
             })
         } else {
-            res.status(404).json('Faltan campos por llenar')
+            res.status(404).json({message:'Faltan campos por llenar'})
         }
     }
     
@@ -177,7 +177,7 @@ export const saved_by = async (req, res) => {
                             }
                         })
                     })
-                    res.status(202).json(savedposts)
+                    res.status(202).json({message:'Ok',savedposts})
                 }).catch(e=>{
                     console.log(e)
                     res.status(404).json('No hay publicaciones')
